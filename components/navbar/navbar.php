db5418a1d5067cd31e7970fb8badfb0001f02cf3
rockyrif <mnamrifky@gmail.com>
2024-02-05 15:47:43 +0530

    stage 1

---
+++ components/navbar/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-md">
+        <div class="container">
+            <a href="" class="navbar-brand fs-4">
+                <img src="../Images/logo.png" alt="Tennis court logo" width="80px" length="80px">
+                <span>Ampara District <span>Tennis</span> Club</span>
+            </a>
+
+            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#myNavBar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="myNavBar">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item ">
+                        <a href="#" class="nav-link active">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+</nav>
