00dc68c63cfedc3aa65a1b2544b8a9083070d484
rockyrif <mnamrifky@gmail.com>
2024-02-12 21:09:54 +0530

    stage 29

--- components/navbar/navbar.php
+++ components/navbar/navbar.php
@@ -1,26 +1,56 @@
-<nav class="navbar navbar-expand-md">
+<!-- Navbar start -->
+<div class="nav-group navbar-dark" id="navbar">
+
+    <nav class="navbar navbar-expand-lg">
         <div class="container">
             <a href="" class="navbar-brand fs-4">
-                <img src="Images/logo.png" alt="Tennis court logo" width="80px" length="80px">
-                <span>Tennis</span> Club Ampara
+                <img src="Images/logo.png" alt="Tennis court logo" width="70px" length="70px">
+                <span class="container responsive-brand-item">
+                    Ampara District<span class="brand-br"><span class="tennis"> Tennis</span> Club</span>
+                </span>
             </a>
 
-            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#myNavBar">
+            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="offcanvas" data-bs-target="#myNavBar">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="myNavBar">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item ">
-                        <a href="#" class="nav-link active">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">About us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Contact</a>
-                    </li>
-                </ul>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="myNavBar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <form class="d-flex mt-1 search " role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"
+                            id="search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item ">
+                            <a href="#" class="nav-link nav-link-home active">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link nav-link-about">ABOUT US</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link nav-link-contact">CONTACT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link nav-link-login">lOG IN</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link nav-link-signup"><button type="button"
+                                    class="btn btn-success">SIGN UP</button></a>
+                        </li>
+                    </ul>
+
+                </div>
             </div>
         </div>
-</nav>
+    </nav>
+</div>
+<!-- Navbar end -->
+
+<!-- local script start -->
+<script src="navbar.js"></script>
+<!-- local script end -->
