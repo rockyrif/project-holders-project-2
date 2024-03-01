b07e10f34d2b03a7c49c9b4d801b1189fb9bdf67
rockyrif <mnamrifky@gmail.com>
2024-03-01 14:16:26 +0530

    stage 42

---
+++ login-and-signup-page/signup.php
@@ -0,0 +1,102 @@
+<?php
+// Include config file
+require_once "config.php";
+
+// Define variables and initialize with empty values
+$username = $email = $password = "";
+$username_err = $email_err = $password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    // Validate name
+    if(empty(trim($_POST["name"]))){
+        $username_err = "Please enter your name.";
+        echo $username_err;
+    } else{
+        $username = trim($_POST["name"]);
+    }
+
+    // Validate email
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter your email.";
+        echo $email_err;
+    } else{
+        $email = trim($_POST["email"]);
+    }
+    
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";
+        echo $password_err;     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have at least 6 characters.";
+        echo $password_err;
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($username_err) && empty($email_err) && empty($password_err)){
+        
+        // Prepare a select statement to check if email is already registered
+        $sql = "SELECT email FROM user_login WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+            
+            // Set parameters
+            $param_email = $email;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) > 0){
+                    $email_err = "This email is already registered.";
+                    echo $email_err;
+                } 
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+
+        // If no email conflict, proceed with inserting into database
+        if(empty($email_err)){
+            // Prepare an insert statement
+            $sql = "INSERT INTO user_login (username, password, email ) VALUES (?, ?, ?)";
+             
+            if($stmt = mysqli_prepare($link, $sql)){
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_password, $param_email, );
+                
+                // Set parameters
+                $param_username = $username;
+                $param_email = $email;
+                $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+                
+                
+                
+                // Attempt to execute the prepared statement
+                if(mysqli_stmt_execute($stmt)){
+                    // Redirect to login page
+                    header("location: ../index.html");
+                } else{
+                    echo "Oops! Something went wrong. Please try again later.";
+                }
+
+                // Close statement
+                mysqli_stmt_close($stmt);
+            }
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
