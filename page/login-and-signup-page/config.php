34e998addb833c08cb9d826525a1d3e7663b91c5
rockyrif <mnamrifky@gmail.com>
2024-03-17 00:20:06 +0530

    stage 42

--- page/login-and-signup-page/config.php
+++ page/login-and-signup-page/config.php
@@ -4,11 +4,11 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'MNAM14059320alocalhost');
 define('DB_NAME', 'amp_dtc');
- 
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+if ($link === false) {
+    die ("ERROR: Could not connect. " . mysqli_connect_error());
 }
