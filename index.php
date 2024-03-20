0552c04495f509be9ca3623e5733529c1adaff20
rockyrif <89009565+rockyrif@users.noreply.github.com>
2024-03-20 08:28:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADTC Home</title>
+    <title>ADTC Home2</title>
 
     <!-- bootstarp start -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
