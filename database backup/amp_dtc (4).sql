76feff0459c33d66af92028a94e177a58d5b005e
rockyrif <mnamrifky@gmail.com>
2024-04-23 10:05:20 +0530

    first

--- database backup/amp_dtc (4).sql
+++ database backup/amp_dtc (4).sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `amp_dtc`
+-- Database: `adtennis_amp_dtc`
 --
 
 -- --------------------------------------------------------
