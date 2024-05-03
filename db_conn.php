cc3db3048d9cf5854b5f00b6bf8927435a964220
rockyrif <mnamrifky@gmail.com>
2024-05-03 21:01:19 +0530

    first

--- db_conn.php
+++ db_conn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "amp_dtc";
+$dbname = "amp_dtc1";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
