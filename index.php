81de3ba3feff2e578ef19d829d482a252620144a
rockyrif <mnamrifky@gmail.com>
2024-05-15 18:26:53 +0530

    first

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>ADTC Home</title>
+    <title>ADTC Home1</title>
 
     <!-- bootstarp start -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
