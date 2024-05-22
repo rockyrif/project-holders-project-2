431485347812e03759d26fe1849398e84a9b5285
rockyrif <mnamrifky@gmail.com>
2024-05-22 10:05:56 +0530

    Added tournament data deletion

--- page/calender/delete.php
+++ page/calender/delete.php
@@ -5,10 +5,11 @@ if (isset($_SESSION["username"]) && $_SESSION["privilage"] === "admin") {
     include $_SERVER['DOCUMENT_ROOT'] . "/project-holders-project-2/db_conn.php";
 
     $id = $_GET['id'];
+    
 
     if (isset($id)) {
         $sql = "DELETE FROM tournament_schedule
-        WHERE $id;";
+        WHERE id = $id;";
     }
 
     if ($conn->query($sql) === TRUE) {
