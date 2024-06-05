0c561a343d2ade2a9ae068ce57869781d9d390c7
rockyrif <mnamrifky@gmail.com>
2024-06-05 09:56:06 +0530

    fixed not login view-gallery not showing error

--- page/gallery-page/view-gallery/view-gallery.php
+++ page/gallery-page/view-gallery/view-gallery.php
@@ -8,7 +8,7 @@ $version = date('Ymd');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gallery</title>
 
     <!-- bootstarp start -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
