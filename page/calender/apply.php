c5c70831e392df6a265cea09c845bb9a22a22c45
rockyrif <mnamrifky@gmail.com>
2024-06-19 13:14:09 +0530

    tounament application error fix

--- page/calender/apply.php
+++ page/calender/apply.php
@@ -32,6 +32,7 @@ if ($_SESSION["loggedin"] = true && isset($_SESSION["username"])) {
                 $new_file_name = $_SESSION["email"] . "-" . $dateTime;
                 // Get file extension
                 $imageFileType = strtolower(pathinfo($_FILES["payment-proof"]["name"], PATHINFO_EXTENSION));
+               
                 // Process file upload
                 $targetFile = $targetDir . $new_file_name . "." . $imageFileType;
                 // Rest of your code for file upload and processing
@@ -44,6 +45,8 @@ if ($_SESSION["loggedin"] = true && isset($_SESSION["username"])) {
             // Check if image file is a valid format and size
             if ($imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "png") {
                 $_SESSION['response'] = "Sorry, only JPG, JPEG, PNG files are allowed.";
+                header('Location: ' . $_SERVER['HTTP_REFERER']);
+                exit;
             } elseif ($_FILES["payment-proof"]["size"] > 500000) { // 500kb limit
                 $_SESSION['response'] = "Sorry, your file is too large. limit to 500kb.";
             } else {
